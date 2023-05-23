89978d562f346217033a3ee9fc8cae16a4b3cfaf
Victor Munyao <vmunyao@AWSBOARD.COM>
2023-05-23 12:52:18 +0300

    complete

--- hello/views.py
+++ hello/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World')
+    return render(request, "hello/index.html")
+
+def munyao(request):
+    return HttpResponse("General Line")
+
+def calc(request):
+    return HttpResponse(1+1)
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
